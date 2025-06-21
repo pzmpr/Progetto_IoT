b4e8c493680b906e0dd009d1a964e450722406e0
pzmpr <114858090+pzmpr@users.noreply.github.com>
2025-06-21 10:44:39 +0200

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -1,7 +1,7 @@
 import face_recognition
 
-known_image = face_recognition.load_image_file("known_image.jpg")
-unknown_image = face_recognition.load_image_file("unknown_image2.jpg")
+known_image = face_recognition.load_image_file("Images/known_image.jpg")
+unknown_image = face_recognition.load_image_file("Images/unknown_image2.jpg")
 
 known_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
