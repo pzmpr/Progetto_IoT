56e1889f1822ac75580af154b54dba523e28aac3
pzmpr <114858090+pzmpr@users.noreply.github.com>
2025-07-18 19:47:36 +0200

    Update motion_detector_server.py

--- src/recording/motion_detector_server.py
+++ src/recording/motion_detector_server.py
@@ -43,7 +43,7 @@ signal.signal(signal.SIGINT, handle_signal)
 conn = psycopg2.connect(
     dbname="Iot",
     user="postgres",
-    password="vivamilan04",
+    password="",
     host="127.0.0.1",
     port="5432"
 )
