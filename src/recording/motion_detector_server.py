b42e6ad3298a1faa23ffe20083b16165b9d3820d
pzmpr <114858090+pzmpr@users.noreply.github.com>
2025-07-18 19:56:14 +0200

    Update motion_detector_server.py

--- src/recording/motion_detector_server.py
+++ src/recording/motion_detector_server.py
@@ -13,21 +13,26 @@ import numpy as np
 import psycopg2
 from flask import Flask, Response, render_template
 
+# Variabili per tempo e percorso file
 date = str(datetime.date.today())
 dest = "Videos/rec-" + date + ".avi"
-fourcc = cv.VideoWriter_fourcc(*'DIVX')
-out = cv.VideoWriter(dest, fourcc, 20, (640,  480))
 
+# Variabili per stream video
+fourcc = cv.VideoWriter_fourcc(*'DIVX')
+out = cv.VideoWriter(dest, fourcc, 10, (640,  480))
+prev_frame = None
+frame = None
 output_frame = None
+
+# Variabili per thread e connessione pagina web
 lock = threading.Lock()
 app = Flask(__name__)
 
+# Variabili connessione mqtt
 host = "127.0.0.1"
 port = 1883
 topic = "Video"
-
-prev_frame = None
-frame = None  # empty variable to store latest message received
+qos = 0
 
 # signal handler
 stop = False
@@ -35,82 +40,91 @@ stop = False
 def handle_signal(signum, frame):
     global stop, app
     stop = True
-    print("\nExiting...")
+    print("\nUscita...")
 
 signal.signal(signal.SIGINT, handle_signal)
 
 # connessione al database
 conn = psycopg2.connect(
-    dbname="Iot",
-    user="postgres",
-    password="",
-    host="127.0.0.1",
-    port="5432"
+    dbname = "Iot",
+    user = "postgres",
+    password = "",
+    host = host,
+    port = "5432"
 )
 cur = conn.cursor()
-print("Connected to database Iot")
-
-def subscribe():
-    global mqttc, stop, dest, date
-    while not stop:
-        mqttc.loop() # Start networking daemon
-    cv.destroyAllWindows()
-    update_db_Registrazioni(date)
-    compress_video(dest, date)
-    os.remove(dest)
-    print("Video compresso")
+print("Connesso al database Iot")
     
-def on_connect(client, userdata, flags, reason_code, properties):  # The callback for when the client connects to the broker
-    global topic
-    client.subscribe(topic)  # Subscribe to the topic, receive any messages published on it
-    print("Subscring to topic :", topic)
+def on_connect(client, userdata, flags, reason_code, properties):
+    global topic, qos
+    client.subscribe(topic, qos)
 
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
     global out, frame, prev_frame, lock, output_frame
     nparr = np.frombuffer(msg.payload, np.uint8)
     frame = cv.imdecode(nparr,  cv.IMREAD_COLOR)
     with lock:
         output_frame = frame.copy()
     out.write(frame)
-    cv.imshow('recv', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        return
+    # cv.imshow('recv', frame)              # mostra stream a schermo
+    # if cv.waitKey(1) & 0xFF == ord('q'):
+    #    return
+
+def subscribe():
+    global mqttc, stop, dest, date
+    while not stop:
+        mqttc.loop()
+    cv.destroyAllWindows()
+    update_db_Registrazioni(date)
+    # chiusura connessione database
+    cur.close()
+    conn.close()
+    compress_video(dest, date)
+    os.remove(dest)
+    print("Video compresso")
+    os._exit(0)
 
 def update_db_Registrazioni(data):
-    nome = "rec-" + data
     cur.execute("""
-                INSERT INTO "Registrazioni"(rec_nome)
+                INSERT INTO "Registrazioni"(data)
                 VALUES (%s)
                 ON CONFLICT DO NOTHING
-                """, (nome,))
+                """, (data,))
     conn.commit()
-    print(f"[✓] Video registrato nel database")
+    print("Video registrato nel database")
 
+# compressione del video
+# si attiva quando il flusso di frame e' terminato (dopo CTRL-C)
 def compress_video(dest, date):
     print("Compressione video...")
     result = ffmpeg.input(dest)
-    result = ffmpeg.output(result, "Videos/rec-"+date+".mp4", bitrate='800k', loglevel='quiet')
+    dest = "Videos/rec-"+date
+    if os.path.exists(dest+".mp4"):
+        i = 1
+        while os.path.exists(f"{dest}({i}).mp4"):
+            i += 1
+        dest = (f"{dest}({i}).mp4")
+    result = ffmpeg.output(result, dest, bitrate='800k', loglevel='quiet')
     ffmpeg.run(result, overwrite_output = True)
 
+# genera i frame per lo stream nella pagina web
 def generate():
     global output_frame, lock
-    # loop over frames from the output stream
     while True:
-        # wait until the lock is acquired
         with lock:
-            # check if the output frame is available, otherwise skip
-            # the iteration of the loop
             if output_frame is None:
-                time.sleep(0.1)
+                # time.sleep(0.1)
                 continue
-            # encode the frame in JPEG formatpietrozmpr@gmail.com
+            # codifica il frame in formatp JPEG
             (flag, encodedImage) = cv.imencode(".jpg", output_frame)
-            # ensure the frame was successfully encoded
+            # assicura che il frame sia stato codificato
             if not flag:
                 continue
-        # yield the output frame in the byte format
+        # restituisci l'immagine come array di byte
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
             bytearray(encodedImage) + b'\r\n')
+
+
 
 @app.route('/')
 def index():
@@ -122,12 +136,12 @@ def video_feed():
         mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect  # Define callback function for successful connection
+mqttc.on_connect = on_connect  # callback function
 mqttc.message_callback_add(topic, on_message)
-mqttc.connect(host,port)  # connecting to the broking server
+mqttc.connect(host,port)  # connessione broker
 
-t = threading.Thread(target=subscribe)    # make a thread to loop for subscribing
-t.start() # run this thread
+t = threading.Thread(target=subscribe) # thread per subscribe
+t.start()
 
 app.run(host="0.0.0.0", port=8000, debug=True,
         threaded=True, use_reloader=False)
